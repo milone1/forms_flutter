150c2875996ac0f324684c77896d79fcc2f01f47
Erick Milan <erickmilan1002@gmail.com>
2022-10-19 22:29:15 -0500

    add ui of form input

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -27,12 +27,52 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox(
                       height: 30,
                     ),
-                    Text('Formulario'),
+                    const _LoginForm()
                   ],
                 ),
               ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Crear una nueva cuenta',
+                style: TextStyle(fontSize: 20),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+                  ),
+                  hintText: 'Email',
+                  labelText: 'Correo Electrónico',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  prefixIcon: Icon(
+                    Icons.alternate_email_sharp,
+                    color: Colors.deepPurple,
+                  )),
+            )
+          ],
         ),
       ),
     );
